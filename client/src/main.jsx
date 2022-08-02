251f9a5eb7ce39944255a8c39e1991a4e1cb5565
vkochukov <vkochukov@appliedtech.ru>
2022-08-02 22:09:06 +0500

    Added transactions context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { TransactionProvider } from "./context/TransactionContext";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+    <TransactionProvider>
+      <App />
+    </TransactionProvider>
+  </React.StrictMode>,
+  document.getElementById("root"),
+);
